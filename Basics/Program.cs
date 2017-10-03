96ef63cccf7024b63e63c49d918602edf88fa65f
kishorkira <kishorkira@gmail.com>
2017-10-03 13:04:50 +0530

    str vs obj

--- Basics/Program.cs
+++ Basics/Program.cs
@@ -6,19 +6,20 @@ namespace Basics
     {
         static void Main(string[] args)
         {
-            Food apple = new Food("Apple");
-            Food apple2 = new Food("Apple");
+            string s1 = "string";
+            string s2 = string.Copy(s1);
+            Object s3 = "object";
+            Object s4 = string.Copy((string)s3);
 
-            Food chocolate = new Food("Chocolate");
-            Console.WriteLine(apple.Equals(chocolate));
-            Console.WriteLine(apple.Equals(apple2));
-            Console.WriteLine(apple == apple2);
+            Console.WriteLine($"ReferenceEqual : {ReferenceEquals(s1,s2)}");
+            Console.WriteLine($"Method : {s1.Equals(s2)}");
+            Console.WriteLine($"Operator : {s1 == s2}");
+            Console.WriteLine($"StaticMethod : {object.Equals(s1,s2)}");
 
-            Console.WriteLine(Equals(apple, apple2));
-            Console.WriteLine(object.Equals(apple,apple2));
-            Console.WriteLine(ReferenceEquals(apple, apple2));
-
-
+            Console.WriteLine($"ReferenceEqual : {ReferenceEquals(s3, s4)}");
+            Console.WriteLine($"Method : {s3.Equals(s4)}");
+            Console.WriteLine($"Operator : {s3 == s4}");
+            Console.WriteLine($"StaticMethod : {object.Equals(s3, s4)}");
 
         }
     }
