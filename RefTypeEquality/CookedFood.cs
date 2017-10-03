a2fa58d729065d3e845b3ef7f2ad01b330b7d372
kishorkira <kishorkira@gmail.com>
2017-10-03 13:48:33 +0530

    Added RefTypeEquality project

---
+++ RefTypeEquality/CookedFood.cs
@@ -0,0 +1,18 @@
+﻿namespace RefTypeEquality
+{
+    public sealed class CookedFood: Food
+    {
+        private string _cookingMethod;       
+
+        public string CookingMethod { get => _cookingMethod; }
+
+        public CookedFood(string name, FoodGroup foodGroup,string cookingMethod) : base(name, foodGroup)
+        {
+            this._cookingMethod = cookingMethod;
+        }
+        public override string ToString()
+        {
+            return $"{_cookingMethod} {Name}";
+        }
+    }
+}
---
+++ RefTypeEquality/Food.cs
@@ -0,0 +1,21 @@
+﻿namespace RefTypeEquality
+{
+    public enum FoodGroup { Meat, Fruit, Vegetables, Sweet }
+    public class Food
+    {
+        private readonly string _name;
+        private readonly FoodGroup _foodGroup;
+        public string Name { get => _name; }
+        public FoodGroup FoodGroup { get => _foodGroup; }
+
+        public Food(string name, FoodGroup foodGroup)
+        {
+            this._name = name;
+            this._foodGroup = foodGroup;
+        }
+        public override string ToString()
+        {
+            return this._name;
+        }
+    }
+}
