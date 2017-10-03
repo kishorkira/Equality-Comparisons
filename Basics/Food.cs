8713a5547e82da07b6a2146d4d799e5953f8731e
kishorkira <kishorkira@gmail.com>
2017-10-03 12:45:56 +0530

    Equals()

--- Basics/Food.cs
+++ Basics/Food.cs
@@ -13,5 +13,17 @@
         {
             return _name;
         }
+        //public override bool Equals(object obj)
+        //{
+        //    if (obj == null)
+        //        return false;
+        //    if (!(obj is Food))
+        //        return false;
+        //    return this._name == ((Food)obj)._name;
+        //}
+        //public override int GetHashCode()
+        //{
+        //    return this._name.GetHashCode();
+        //}
     }
 }
--- Basics/Program.cs
+++ Basics/Program.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace Basics
 {
@@ -11,25 +6,19 @@ namespace Basics
     {
         static void Main(string[] args)
         {
-            Button btn1 = new Button
-            {
-                Text = "Button"
-            };
-            Button btn2 = new Button
-            {
-                Text = "Button"
-            };
+            Food apple = new Food("Apple");
+            Food apple2 = new Food("Apple");
 
-            Console.WriteLine(btn1 == btn2);
-            Console.WriteLine(btn1.Equals(btn2));
+            Food chocolate = new Food("Chocolate");
+            Console.WriteLine(apple.Equals(chocolate));
+            Console.WriteLine(apple.Equals(apple2));
+            Console.WriteLine(apple == apple2);
 
-            float f1 = 4.0000001f;
-            float f2 = 4.0000000f;
-            Console.WriteLine(f1 == f2);
-            float f3 = 5.075f;
-            float f4 = 1.025f;
-            Console.WriteLine(f3 + f4);
-            Console.WriteLine((f3 + f4) == 6.1f);
+            Console.WriteLine(Equals(apple, apple2));
+            Console.WriteLine(object.Equals(apple,apple2));
+            Console.WriteLine(ReferenceEquals(apple, apple2));
+
+
 
         }
     }
