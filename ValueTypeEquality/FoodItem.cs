7151ccbe73b04beb02d78bc9729d5c7f1232e648
kishorkira <kishorkira@gmail.com>
2017-10-03 13:12:38 +0530

    Added ValueTypeEquality project

---
+++ ValueTypeEquality/FoodItem.cs
@@ -0,0 +1,21 @@
+﻿namespace ValueTypeEquality
+{
+    public enum FoodGroup { Meat, Fruit, Vegetables, Sweet}
+    public struct FoodItem
+    {
+        private readonly string _name;
+        private readonly FoodGroup _foodGroup;
+        public string Name { get => _name; }
+        public FoodGroup FoodGroup { get => _foodGroup; }
+
+        public FoodItem(string name, FoodGroup foodGroup)
+        {
+            this._name = name;
+            this._foodGroup = foodGroup;
+        }
+        public override string ToString()
+        {
+            return this._name;
+        }
+    }
+}
