c515b7df32fa1f9d252be9e7a6ba67aafdfbd436
kishorkira <kishorkira@gmail.com>
2017-10-03 11:05:15 +0530

    Added Food.cs

---
+++ Basics/Food.cs
@@ -0,0 +1,17 @@
+﻿namespace Basics
+{
+    public class Food
+    {
+        string _name;
+        public string Name { get => _name; }
+
+        public Food(string name)
+        {
+           this._name = name;
+        }
+        public override string ToString()
+        {
+            return _name;
+        }
+    }
+}
