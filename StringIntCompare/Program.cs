05ad954f47437b6b834cc9d71139409ac17a2171
kishorkira <kishorkira@gmail.com>
2017-10-04 11:09:04 +0530

    IComparable<T>

--- StringIntCompare/Program.cs
+++ StringIntCompare/Program.cs
@@ -10,6 +10,30 @@ namespace StringIntCompare
     {
         static void Main(string[] args)
         {
+            string apple = "apple";
+            string pear = "pear";
+
+            Console.WriteLine(apple.CompareTo(apple));
+            Console.WriteLine(apple.CompareTo(pear));
+            Console.WriteLine(pear.CompareTo(apple));
+
+            DisplayOrder(apple, apple);
+            DisplayOrder(apple, pear);
+            DisplayOrder(pear, apple);
+
+
+        }
+
+        private static void DisplayOrder<T>(T x,T y) where T : IComparable<T>
+        {
+            int result = x.CompareTo(y);
+            if(result == 0)
+                Console.WriteLine($"{x,10} = {y}");
+            if(result > 0)
+                Console.WriteLine($"{x,10} > {y}");
+            if(result < 0)
+                Console.WriteLine($"{x,10} < {y}");
+
         }
     }
 }
