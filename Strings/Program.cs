b73529ba0da9b5af42dc36b0a3038a7c435393aa
kishorkira <kishorkira@gmail.com>
2017-10-04 14:56:26 +0530

    Strings WB

--- Strings/Program.cs
+++ Strings/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Strings
 {
@@ -10,6 +6,31 @@ namespace Strings
     {
         static void Main(string[] args)
         {
+            int ordinalResult = string.Compare("apple", "Apple", StringComparison.Ordinal);
+            int ordinalIgnoreCaseResult = string.Compare("apple", "Apple", StringComparison.OrdinalIgnoreCase);
+            
+            //Console.WriteLine($"Ordinal Compare : {ordinalResult}");
+            //Console.WriteLine($"OrdinalIgnoreCase Compare : {ordinalIgnoreCaseResult}");
+
+            DisplayComparision("apple", "Apple", StringComparison.OrdinalIgnoreCase);
+            DisplayComparision("apple", "Apple", StringComparison.Ordinal);
+
+        }
+
+        private static void DisplayComparision(string s1, string s2, StringComparison comparison)
+        {
+            int result = string.Compare(s1, s2, comparison);
+            Console.WriteLine($"{s1} {GetCompareSymbol(result)} {s2} ({result},{comparison})");
+        }
+
+        private static string GetCompareSymbol(int result)
+        {
+            if (result == 0)
+                return "==";
+            if (result > 0)
+                return ">";
+            else
+                return "<";
         }
     }
 }
